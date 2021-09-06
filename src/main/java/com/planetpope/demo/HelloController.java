a8e5f24c8a97fcb1be212990cd7f8fea6622cf38
Myat Su Mon <myat86@gmail.com>
2021-09-06 16:32:24 +0800

    Update the hello controller

--- src/main/java/com/planetpope/demo/HelloController.java
+++ src/main/java/com/planetpope/demo/HelloController.java
@@ -1,4 +1,4 @@
-package com.planetpope.demo;
+//package com.planetpope.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
